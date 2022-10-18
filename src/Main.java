35320742af0b6f1536d6d1e153cf382e9901fd52
Alexandre Massao <alexandremsubs@gmail.com>
2022-10-18 09:37:59 -0300

    adicionada função de pesquisar doutor por especialidade e nome
    também incluido metodos para pesquisar e exibir doutores por nome e especialidade

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main {
         do {
             System.out.println(
                     """
+                    
                     Sistema Hospital
                     1 - Adicionar Paciente
                     2 - Remover Paciente
@@ -26,7 +27,7 @@ public class Main {
                     5 - Exibir Pacientes
                     6 - Adicionar Doutor
                     7 - Remover Doutor
-                    8 - Exibir Doutores (debug)
+                    8 - Pesquisar Doutor
                     
                     Digite SAIR para Sair.
                     """);
@@ -54,21 +55,23 @@ public class Main {
                     );
 
                     hospital.adicionarPaciente(paciente);
+
                     break;
 
                 //Remover Paciente
                 case "2":
                     System.out.println("""
+                            
                             Remover Paciente
                             
-                            1 - Pesquisar pelo CPF
+                            A - Pesquisar pelo CPF
                             Qualquer outro caractere para voltar para o menu
                             """);
 
                     System.out.print("Escolha: ");
                     escolha = leia.next().toUpperCase();
 
-                    if(escolha.equals("1")){
+                    if(escolha.equals("A")){
                         System.out.print("CPF: ");
                         cpf = leia.next();
                         indice = hospital.pesquisarPacienteCpf(cpf);
@@ -86,62 +89,68 @@ public class Main {
                     }else {
                         System.out.println("Cancelado!");
                     }
+
                     break;
 
                 //Pesquisar Paciente
                 case "3":
                     System.out.println("""
+                            
                             Pesquisar Paciente
-                            1 - Pesquisar pelo nome
-                            2 - Pesquisar pelo CPF
+                            A - Pesquisar pelo nome
+                            B - Pesquisar pelo CPF
+                            
                             Qualquer outro caractere para voltar para o menu
                             """);
                     System.out.print("Sua escolha: ");
-                    escolha = leia.next();
+                    escolha = leia.next().toUpperCase();
 
-                    if (escolha.equals("1")){
+                    if (escolha.equals("A")){
                         System.out.print("Nome: ");
                         nome = leia.next();
                         hospital.exibirPacientesComNome(nome);
 
-                    } else if (escolha.equals("2")) {
-                        System.out.println("CPF: ");
+                    } else if (escolha.equals("B")) {
+                        System.out.print("CPF: ");
                         cpf = leia.next();
-                        indice = hospital.pesquisarPacienteCpf(cpf);
-                        hospital.getPacientes().get(indice).print();
+                        hospital.exibirPacientesComCpf(cpf);
 
                     } else {
                         System.out.println("Cancelado!");
                     }
 
+                    break;
+
                 //Editar Paciente
                 case "4":
                     System.out.println("""
+                            
                             Editar Paciente
-                            1 - Pesquisar pelo CPF
+                            A - Pesquisar pelo CPF
                             Qualquer outro caractere para voltar para o menu""");
 
                     System.out.print("Sua escolha: ");
                     escolha = leia.next();
 
-                    if (escolha.equals("1")) {
+                    if (escolha.equals("A")) {
+                        System.out.print("CPF");
                         cpf = leia.next();
                         indice = hospital.pesquisarPacienteCpf(cpf);
 
-                        System.out.println("Encontrado: ");
+                        System.out.print("Encontrado: ");
                         hospital.getPacientes().get(indice).print();
 
                         System.out.print("Deseja modificar? S/N: ");
                         escolha = leia.next().toUpperCase();
 
                         if(escolha.equals("S")){
-                            System.out.println("Nome: ");
+                            System.out.print("Nome: ");
                             nome = leia.next();
-                            System.out.println("Idade: ");
+                            System.out.print("Idade: ");
                             idade = leia.nextInt();
-                            System.out.println("CPF: ");
+                            System.out.print("CPF: ");
                             cpf = leia.next();
-                            System.out.println("Data nascimento: ");
+                            System.out.print("Data nascimento: ");
                             dataNascimento = leia.next();
 
                             hospital.editarPaciente(
@@ -157,9 +166,12 @@ public class Main {
                         System.out.println("Cancelado!");
                     }
 
+                    break;
+
                     //Exibir Pacientes
                 case "5":
                     hospital.exibirTodosPacientes();
+
                     break;
 
                     //Adicionar Doutor
@@ -175,18 +187,19 @@ public class Main {
                     );
 
                     hospital.adicionarDoutor(doutor);
+
                     break;
 
                     //Remover Doutor
                 case "7":
                     ArrayList<Integer> doutoresEncontrados;
                     int indiceParaRemocao;
+
                     System.out.print("Nome: ");
                     nome = leia.next();
 
                     //esse metodo retorna cada posição do indice da array doutores que o nome foi encontrado
                     doutoresEncontrados = hospital.pesquisarDoutorNome(nome);
-                    System.out.println(doutoresEncontrados);
 
                     //este loop printa todas as vezes que o nome foi encontrado
                     for(int doutorEncontrado : doutoresEncontrados){
@@ -210,14 +223,35 @@ public class Main {
                         System.out.println("Cancelado");
                     }
 
-
                     break;
 
-                    //Exibir Doutores (debug)
+                    //Pesquisar Doutores
                 case "8":
-                    hospital.exibirTodosDoutores();
-                    break;
+                    System.out.println("""
+                            A - Pesquisar por Nome
+                            B - Pesquisar por Especialidade
+                            
+                            Qualquer outro caractere para sair
+                            """);
 
+                    System.out.print("Sua escolha: ");
+                    escolha = leia.next();
+
+                    if (escolha.equals("A")){
+                        System.out.print("Nome: ");
+                        nome = leia.next();
+
+                        hospital.exibirDoutoresComNome(nome);
+                    } else if (escolha.equals("B")){
+                        System.out.print("Especialidade: ");
+                        especialiade = leia.next();
+
+                        hospital.exibirDoutoresComEspecialidade(especialiade);
+                    }else {
+                        System.out.println("Cancelado");
+                    }
+
+                    break;
             }
         }while (!escolha.equals("SAIR"));
     }
